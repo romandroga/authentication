0e5f0ad7ff76d6f69eb12a73502e7336100a4d71
Roman Droga <romandroga@gmail.com>
2020-12-03 14:44:51 +0200

    modified:   index.js

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const server = require("./server");
 const config = require("./config");
 
-server(config.PORT, (err) => {
+
+const port = config.PORT || 3001;
+
+server(port, (err) => {
   if (err) {
     console.log("Error on listen :", err);
     process.exit(1);
   }
-  console.log(`Server is alive on port ${config.PORT}`);
+  console.log(`Server is alive on port ${port}`);
 });
